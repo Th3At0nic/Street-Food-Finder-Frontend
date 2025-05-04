9f97ad980061dcb201ad575be5e5efeff3f785f8
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-05-04 12:20:46 +0600

    login and registation some code added

---
+++ src/components/modules/auth/login/login.tsx
@@ -0,0 +1,141 @@
+"use client";
+import ReCAPTCHA from "react-google-recaptcha";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Link from "next/link";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { toast } from "sonner";
+
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { loginSchema } from "./loginValidation";
+import {
+  loginUser,
+  reCaptchaTokenVerification,
+} from "@/components/services/AuthService";
+import { Utensils } from "lucide-react";
+
+export default function LoginForm() {
+  const searchParams = useSearchParams();
+  const redirect = searchParams.get("redirectPath");
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const [reCaptchaStatus, setReCaptchaStatus] = useState(false);
+
+  const {
+    formState: { isSubmitting },
+  } = form;
+
+  const handleReCaptcha = async (value: string | null) => {
+    try {
+      const res = await reCaptchaTokenVerification(value!);
+      if (res?.success) {
+        setReCaptchaStatus(true);
+      }
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    try {
+      const res = await loginUser(data);
+      if (res?.success) {
+        toast.success(res?.message);
+        if (redirect) {
+          router.push(redirect);
+        } else {
+          router.push("/");
+        }
+      } else {
+        toast.error(res?.message);
+      }
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-5">
+      <div className="flex items-center space-x-4 ">
+        <Link
+          href="/"
+          className="flex items-center gap-2 text-orange-600 font-bold text-xl"
+        >
+          <Utensils className="h-6 w-6" />
+          StreetBites
+        </Link>
+        <div>
+          <h1 className="text-xl font-semibold">Login</h1>
+          <p className="font-extralight text-sm text-gray-600">Welcome back!</p>
+        </div>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex mt-3 w-full">
+            <ReCAPTCHA
+              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_CLIENT_KEY || ""}
+              onChange={handleReCaptcha}
+              className="mx-auto"
+            />
+          </div>
+
+          <Button
+            disabled={reCaptchaStatus ? false : true}
+            type="submit"
+            className="mt-5 w-full bg-orange-600 hover:bg-orange-50 hover:text-orange-600 cursor-pointer"
+          >
+            {isSubmitting ? "Logging...." : "Login"}
+          </Button>
+        </form>
+      </Form>
+      <p className="text-sm text-gray-600 text-center my-3">
+        Do not have any account ?
+        <Link href="/register" className="text-primary">
+          Register
+        </Link>
+      </p>
+    </div>
+  );
+}
