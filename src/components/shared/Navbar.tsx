cc5bbb1378c1f611c137dc3c013e178da3d511db
SMTamim <tamimmahmud0@gmail.com>
2025-05-05 17:25:51 +0600

    add: logout button

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,8 +1,14 @@
-import { Sparkles, Utensils } from "lucide-react";
+"use client";
+
+import { LogOut, Sparkles, Utensils } from "lucide-react";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { signOut, useSession } from "next-auth/react";
+import { signOutUser } from "@/lib/auth/signOutUser";
 
 export default function Navbar() {
+  const { status } = useSession();
+
   return (
     <nav className="bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
@@ -21,9 +27,20 @@ export default function Navbar() {
             <Sparkles className="h-4 w-4 mr-2" />
             Go Premium
           </Button>
-          <Link href={'/signup'}>
-            <Button variant="outline">Sign In</Button>
-          </Link>
+          {
+            status === 'authenticated' ? (
+              <div className="flex justify-center items-center">
+                <Link href={'/dashboard'}>
+                  <Button variant="outline">Dashboard</Button>
+                </Link>
+                <Button onClick={() => signOutUser()} variant="outline"><LogOut className="h-4 w-4" /></Button>
+              </div>
+            ) :
+              <Link href={'/signup'}>
+                <Button variant="outline">Sign In</Button>
+              </Link>
+          }
+
         </div>
       </div>
     </nav>
