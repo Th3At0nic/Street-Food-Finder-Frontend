851b7d71d521f987ac44daec0217fd31347172b1
SMTamim <tamimmahmud0@gmail.com>
2025-05-05 17:11:45 +0600

    added eslint rules

--- src/components/modules/auth/register/RegisterForm.tsx
+++ src/components/modules/auth/register/RegisterForm.tsx
@@ -50,7 +50,7 @@ export default function RegisterForm() {
       } else {
         toast.error(res?.message);
       }
-    } catch (err: any) {
+    } catch (err: unknown) {
       console.error(err);
     }
   };
